e54d392642fc9ccc6b54c6b384346ec941e9fe48
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2019-03-17 02:22:37 +0530

    node packages modified

--- nova/server.js
+++ nova/server.js
@@ -3,6 +3,7 @@
 const http= require('http');
 const hostname='127.0.0.1';
 const port=3000;
+const mongoose= require('mongoose');
 const server= http.createServer(function(req,res){
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -12,3 +13,18 @@ const server= http.createServer(function(req,res){
 server.listen(port, hostname, function(){
   console.log("Server running successfully");
 })
+// const mongo = require('mongodb').MongoClient
+// function doConnect() {
+//  	var url = 'mongodb://Abhirupa:abhirupa123@ds261527.mlab.com:61527/user_nova'
+// 	mongo.connect(url, (err, db) => {
+// 		if(err) {
+// 			throw err
+// 		} else {
+// 			console.log('Successfully connected to MongoDB')
+// 		}
+// 	})
+// }
+ mongoose.connect('mongodb://Abhirupa:abhirupa123@ds261527.mlab.com:61527/user_nova');
+ mongoose.connection.once('open',() =>{
+   console.log("Nova has successfully connected to the database");
+ })
